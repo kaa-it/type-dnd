e5bbff125d6baca41126f9f9188166c8c80cafd8
Andrey Kruglov <kruglov.it@gmail.com>
2024-10-07 17:42:17 +0300

    feat: fixes

---
+++ src/utils/types.ts
@@ -0,0 +1,17 @@
+export type TIngredient = {
+    _id: string;
+    name: string;
+    type: string;
+    proteins: number;
+    fat: number;
+    carbohydrates: number;
+    calories: number;
+    price: number;
+    image: string;
+    image_large: string;
+    image_mobile: string;
+  };
+
+  export type TConstructorIngredient = TIngredient & {
+    id: string;
+  };
